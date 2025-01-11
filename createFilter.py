957ca522acf4336ae984032c44640b4e2e1fbfb4
wenlong <wenlonglwl@163.com>
2025-01-11 16:23:24 +0800

    poe2

--- createFilter.py
+++ createFilter.py
@@ -1,56 +1,43 @@
 
-maps = {"Class":"Maps", "BaseType":"", "MapTier":"", }
+colors = {}
+mapStyle = {1:{"PlayEffect":"Purple", 
+               "MinimapIcon":"0 Orange Circle", 
+               "SetTextColor":"200 200 200 255",
+               "SetBackgroundColor":"0 0 0 240"},
+               "SetBorderColor":"229 23 23 255",
+               "SetFontSize":"45"}
+def getMapStyle(value):
+    if value == 1:
+        return "    SetTextColor 0 0 0\n\
+    SetBackgroundColor 255 255 255\n\
+    SetBorderColor 128 128 128\n\
+    SetFontSize 50\n\
+    DisableDropSound True\n\
+    CustomAlertSound \"文子过滤音效\高级地图.mp3\"\n\
+    MinimapIcon 0 Pink Hexagon\n\
+    PlayEffect Pink\n"
 
-class Info:
-    '过滤信息'
+def processMaps(mapFiter, mapLine):
+    infos = mapLine.split("#")[:-1]
+    print(infos)
+    fiterInfo = 'Show\n'
+    fiterInfo = fiterInfo + "    Class " + infos[1] + " #" + infos[4] + "\n"
+    fiterInfo = fiterInfo + "    Class " + infos[1] + " #" + infos[4] + "\n"
+    fiterInfo = fiterInfo + "    BaseType " + infos[2] + "\n"
+    print(fiterInfo)
+    fiterInfo = fiterInfo + getMapStyle(int(infos[3]))
+    mapFiter.append(fiterInfo)
 
-    def __init__(self, value):
-        self.__value = value
-        self.__Class = ""
-        self.__BaseType = ""
-        self.__SetTextColor = ""
-        self.__SetBackgroundColor = ""
-        self.__setBorderColor = ""
-        self.__SetFontSize = ""
-        #DisableDropSound
-        self.__Minimaplcon = ""
-        self.__PlayEffect = ""
-        self.__CustomAlertSound = ""
-        self.setStyle()
-    
-    def setStyle(self):
-        if self.__value == 1:
-            pass
-        elif self.__value == 2:
-            pass
-        elif self.__value == 3:
-            pass
-        elif self.__value == 4:
-            pass
-        elif self.__value == 5:
-            pass 
-        elif self.__value == 0:
-            pass
-    
-    def printInfo(self):
-        string = "Show\n  Class " + self.__Class
-        pass
+def readItems():
+    with open("./items.txt", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    fiter = []
+    for line in lines:
+        if line.startswith("#"):
+            continue
+        if line.startswith("地图"):
+            processMaps(fiter, line)
+    with open("./fiter.txt", 'w', encoding="utf-8") as f:
+        f.writelines(fiter)
 
-class Map:
-    '地图类'
-
-    def __init__(self):
-        self.maps = []
-        self.special = ["Pit of the Chimera Map", "Lair of the Hydra Map", \
-                        "Maze of the Minotaur Map", "Maze of the Minotaur Map", "Harbinger Map",\
-                        "Engraved Ultimatum", "Vaal Temple Map"]
-        self.info = Info(-1)
-
-    def processMaps(self):
-        '四守卫地图、征服者地图、裂界守卫地图、瓦尔密殿、意境地图、传奇地图、T1456、红图、黄图、白图'
-        pass
-
-#class                 
-
-if __name__ == "__main__":
-    pass
+readItems()
